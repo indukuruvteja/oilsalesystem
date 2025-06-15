4eecc37a8d11f43abb715db74ff6e99111d5d4a5
indukuruvteja <indukuruvteja@gmail.com>
2025-06-15 22:51:40 +0530

    Update CustomerController.java

--- src/main/java/com/example/oilsalesystem/controller/CustomerController.java
+++ src/main/java/com/example/oilsalesystem/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+    
 
     @Autowired
     private CustomerService customerService;
